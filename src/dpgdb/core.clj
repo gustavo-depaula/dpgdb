6e6ed6f6a3c8389f046c70a899f0c2d9058c4436
Gustavo de Paula <gustavodepaula@disroot.org>
2020-11-22 21:25:14 -0300

    feat: major compact refact and merge

--- src/dpgdb/core.clj
+++ src/dpgdb/core.clj
@@ -4,6 +4,14 @@
 
 (def MAX-SEGMENT-ENTRIES 25)
 (def segment-counter (atom {0 0}))
+(defn add-new-segment []
+  (let [new-segment-index (->> @segment-counter
+                               keys
+                               (apply max)
+                               inc)]
+    (swap! segment-counter assoc new-segment-index 0)
+    new-segment-index))
+
 (def entries-refs (atom {}))
 
 (defn read-nth-line
@@ -15,7 +23,9 @@
 (defn get-segment-filename [index]
   (str "db-files/segment-" index ".log"))
 
-(defn make-entry [key value] (str key " " value "\n"))
+(defn make-entry
+  ([key value] (str key " " value "\n"))
+  ([line] (str line "\n")))
 (defn get-entry-key [entry] (get (str/split entry #"\ +") 0))
 (defn get-entry-value [entry] (get (str/split entry #"\ +") 1))
 
@@ -53,36 +63,48 @@
                                    :segment segment-index})
     (swap! segment-counter assoc segment-index appended-line)))
 
-(defn kv-dict-in-logfile [index]
+(defn save-entries [entries]
+  (doseq [entry entries] (save-entry entry)))
+
+(defn read-entries-in-segment [index]
   (let [segment-filename (get-segment-filename index)
         file-content (slurp segment-filename)
-        lines (str/split file-content #"\n")
-        kv-pairs (map
-                  (fn  [line]
-                    (let [breakdown (str/split line #"\ +")
-                          key (get breakdown 1)
-                          value (get breakdown 2)]
-                      (vec [key value])))
-                  lines)
-        values-dict (into (sorted-map) kv-pairs)]
-    values-dict))
+        lines (str/split file-content #"\n+")
+        entries (map make-entry lines)]
+    entries))
 
-(defn keys-in-segment [index]
-  (let [refs (deref entries-refs)
-        keys (keys refs)
-        keys-in-segment (filter #(= (str index)  (str (:segment (get refs %)))) keys)]
-    keys-in-segment))
+(defn compact-entries-list [entries]
+  (let [entries-map (reduce #(assoc %1 (get-entry-key %2) %2)
+                            {}
+                            entries)
+        compacted-entries (vals entries-map)]
+    compacted-entries))
 
-(defn compact-file [index]
-  (let [kv-pairs-in-file (kv-dict-in-logfile index)
-        ks-in-segment (keys-in-segment index)
+(defn get-merged-segments-entries [& indexes]
+  (let [entries-lists (map read-entries-in-segment indexes)
+        entries-list (apply concat entries-lists)
+        merged-entries-list (compact-entries-list entries-list)]
+    merged-entries-list))
 
-        segments (keys (deref segment-counter))
-        max-segment (apply max segments)]
-    (swap! segment-counter assoc (inc max-segment) 0)
-    (doseq [keyval kv-pairs-in-file] (save-entry (key keyval) (val keyval)))
-    (clojure.java.io/delete-file (get-segment-filename index))
-    (println (str "compacted segment file " (get-segment-filename index) " into " (get-segment-filename (inc max-segment))))))
+(defn delete-segment-files [& indexes]
+  (doseq [index indexes] (clojure.java.io/delete-file (get-segment-filename index))))
+
+(defn merge-segments [& indexes]
+  (let [entries (apply get-merged-segments-entries indexes)
+        new-segment-index (add-new-segment)]
+    (save-entries entries)
+    (apply delete-segment-files indexes)
+    (println (str "merged segments " indexes " into new segment" new-segment-index))))
+
+(defn compact-segment [index]
+  (let [entries (-> index
+                    read-entries-in-segment
+                    compact-entries-list)
+        new-segment-index (add-new-segment)]
+    (save-entries entries)
+    (delete-segment-files index)
+    (println (str "compacted segment file " (get-segment-filename index) " into " (get-segment-filename new-segment-index)))))
+
 
 (defn process-command [command args]
   (when (= command "set")
@@ -95,7 +117,10 @@
       (println (read-entry key))))
   (when (= command "compact")
     (let [index (get args 0)]
-      (compact-file index))))
+      (compact-segment index)))
+  (when (= command "merge")
+    (let [indexes (subvec args 1)]
+      (merge-segments indexes))))
 
 (defn repl []
   (while true
