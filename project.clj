38ef0d76ff9580adf97259f622722b7189af1e2b
Gustavo de Paula <gustavodepaula@disroot.org>
2020-12-05 17:29:21 -0300

    docs: add license

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject dpgdb "0.1.0-SNAPSHOT"
+(defproject dpgdb "0.1.0"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :url "https://github.com/gustavo-depaula/dpgdb"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot dpgdb.core
   :target-path "target/%s"
